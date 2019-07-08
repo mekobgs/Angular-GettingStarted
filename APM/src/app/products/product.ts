f0bfcb3b0be3431331b8d3165a15345b6c9a72b3
Elmer Chacon <treechacon@crecer.com.sv>
2019-07-08 17:38:21 -0600

    New components, new pipes

---
+++ APM/src/app/products/product.ts
@@ -0,0 +1,12 @@
+import { CONSTRUCTOR } from '@angular/compiler-cli/src/ngcc/src/host/fesm2015_host';
+export interface IProduct{
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
+
