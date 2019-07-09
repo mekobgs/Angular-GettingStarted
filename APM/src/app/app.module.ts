d8c53476c1029f632e6d2b57e1bc8cc9c66202b4
Elmer Chacon <treechacon@crecer.com.sv>
2019-07-09 14:23:55 -0600

    Finishing Routing First Part

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -6,6 +8,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 
 @NgModule({
@@ -13,12 +16,21 @@ import { AppComponent } from './app.component';
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent 
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { IProduct } from './product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector:'pm-products',
     templateUrl:'./product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
